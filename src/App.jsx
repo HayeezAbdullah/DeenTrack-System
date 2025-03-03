c798b182ede098563ae570af0913b3d9807cdd2d
Hayeez Abdullah <hayeezabdullah2k5@gmail.com>
2025-03-03 17:59:01 +0530

    Redesign & Restructure Cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,47 @@ import SingleCard from "./Singlecard";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-[#F8F5E9]">
+    <div className="min-h-screen bg-[#F7F7F7]">
       <Navbar />
       <SearchBar />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 p-4">
+      <div className="hidden md:grid md:grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 p-4">
         <SingleCard type={"Vegetables"} imglink={"./veg.png"} />
         <SingleCard type={"Cooking Essentials"} imglink={"./cooking.png"} />
         <SingleCard type={"Bakery & Dairy"} imglink={"./dairy.png"} />
         <SingleCard type={"Grain Items"} imglink={"./grains.png"} />
         <SingleCard type={"Household Items"} imglink={"./household.png"} />
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 p-4">
-        <Productcard
-          product="Carrot (250g)"
-          price="150"
-          image="https://th.bing.com/th/id/OIP.wlej91eS1mnUwFVeDW25ggHaHa?rs=1&pid=ImgDetMain"
-          onClick={() => console.log("Added to cart")}
-        />
-        <Productcard
-          product="Kottumee"
-          price="130"
-          image="https://lmd.lk/wp-content/uploads/2021/12/KM-Seasonal-Pack.jpg"
-          onClick={() => console.log("Added to cart")}
-        />
-        <Productcard
-          product={"Kotmale Fresh Milk"}
-          price="550"
-          image="https://lankaface.com/wp-content/uploads/2022/06/kotmale-full-cream-fresh-milk.png"
-          onClick={() => console.log("Added to cart")}
-        />
+      <div className="container mt-5 mx-auto">
+        <h2 className="text-xl  text-center mb-6 text-gray-700">
+          Deal of the Day
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 ">
+          <Productcard
+            product="Carrot (250g)"
+            price="150"
+            image="./carrot.png"
+            onClick={() => console.log("Added to cart")}
+          />
+          <Productcard
+            product="Kottumee"
+            price="130"
+            image="./kottumee.png"
+            onClick={() => console.log("Added to cart")}
+          />
+          <Productcard
+            product={"Kotmale Fresh Milk"}
+            price="550"
+            image="./kotmale_fresh_milk.png"
+            onClick={() => console.log("Added to cart")}
+          />
+          <Productcard
+            product={"Ratthi 200g"}
+            price="555"
+            image="./ratthi.png"
+            onClick={() => console.log("Added to cart")}
+          />
+        </div>
       </div>
     </div>
   );
--- src/Productcard.jsx
+++ src/Productcard.jsx
@@ -1,24 +1,28 @@
+import { ShoppingCart } from "lucide-react";
+
 const Productcard = ({ product, price, image, onClick }) => {
   return (
     <div
-      className="rounded-lg shadow-md hover:scale-101 hover:bg-green-50 transform transition duration-300 cursor-pointer overflow-hidden"
+      className="rounded-lg shadow-md transform transition duration-300 overflow-hidden  hover:bg-green-50  flex items-center"
       onClick={onClick}
     >
-      {/* Image Section */}
-      <div className="w-full h-40">
-        <img src={image} alt={product} className="w-full h-full object-cover" />
+      {/* Image on the Left */}
+      <div className="w-2/5">
+        <img src={image} alt={product} className="w-full h-40 object-cover" />
       </div>
 
-      {/* Product Name + Price */}
-      <div className="p-4 flex flex-col justify-between h-40">
-        <div className="mb-2">
-          <h2 className="text-xl font-bold text-gray-700">{product}</h2>
-          <h2 className="text-3xl font-bold text-gray-900">{price}/=</h2>
+      {/* Details on the Right */}
+      <div className="w-3/5 p-4 flex flex-col justify-between h-full">
+        <div>
+          <h2 className=" font-bold text-gray-800">{product}</h2>
+          <h2 className=" font-semibold text-green-700 my-2">
+            Price: {price}/=
+          </h2>
         </div>
 
-        {/* Button Section */}
-        <button className="bg-gray-800 text-white py-2 px-4 rounded-full hover:bg-green-800 transition">
-          Add to Cart
+        <button className="bg-gray-800 text-white flex items-center gap-2 justify-center py-2 px-4 rounded-full hover:bg-green-800 transition text-sm cursor-pointer">
+          <ShoppingCart size={18} />
+          Purchase
         </button>
       </div>
     </div>
